f425c630806301033cfdb467e1bd8f233d8ebc83
Rytis Karpuška <rytis.karpuska@nordsec.com>
2025-04-06 12:44:20 +0300

    Add converter from RH to Template types, add tests as well

---
+++ converter.py
@@ -0,0 +1,213 @@
+from RHRaceStatusTypes import RaceStatus
+from RHHeatTypes import Heats as RHHeats
+from RHPilotTypes import Pilots as RHPilots
+from RHResultTypes import Results as RHResults
+from RHClassTypes import Classes as RHClasses
+from RHFrequencyTypes import Frequencies as RHFrequencies, Frequency as RHFrequency
+from TemplateTypes import (
+    CurrentHeat as TCurrentHeat,
+    CurrentHeatPilot as TCurrentHeatPilot,
+    CurrentHeatPilots as TCurrentHeatPilots,
+    PilotResults as TPilotResults,
+    PilotResult as TPilotResult,
+    Round as TRound,
+    Rounds as TRounds,
+    Heat as THeat,
+    HeatPilot as THeatPilot
+)
+
+from typing import Optional
+
+NO_DATA = "N/A"
+EMPTY = ""
+BASE_POINTS = 1000
+
+def frequency_to_str(f: RHFrequency):
+    return f"{f.band}{f.channel} ({f.frequency} Mhz)"
+
+def value_or_default(v, d):
+    if v is not None:
+        return v
+    else:
+        return d
+
+def map_or_default(m, v, d):
+    if v is None:
+        return d
+
+    if v not in m:
+        return d
+
+    return m[v]
+
+def current_heat(rc: Optional[RaceStatus], heats: Optional[RHHeats]) -> TCurrentHeat:
+    if rc is None or heats is None:
+        return "N/A"
+    heat_id = rc.race_heat_id
+    target = next((h for h in heats.heats if h.id == heat_id), None)
+    if target is None:
+        return NO_DATA
+    return target.displayname
+
+def current_pilots(rc: Optional[RaceStatus], h: Optional[RHHeats], p: Optional[RHPilots], f: Optional[RHFrequencies]) -> TCurrentHeatPilots:
+    if rc is None or h is None or p is None or f is None:
+        return []
+
+    heat_id = rc.race_heat_id
+    current_heat = next((h for h in h.heats if h.id == heat_id), None)
+    if current_heat is None:
+        return []
+
+    slots = sorted(current_heat.slots, key=lambda s: s.node_index)
+    pilot_ids = [s.pilot_id for s in slots]
+
+    def find_pilot_by_id(pilot_id: int):
+        assert p
+        return next((p.callsign for p in p.pilots if p.pilot_id == pilot_id), "")
+
+    return [TCurrentHeatPilot(
+            nickname=find_pilot_by_id(pilot_id),
+            channel = frequency_to_str(f.fdata[channel_id]))
+            for channel_id, pilot_id in enumerate(pilot_ids) if len(f.fdata) > channel_id and f.fdata[channel_id].band is not None]
+
+def pilot_results(r: Optional[RHResults], h: Optional[RHHeats], p: Optional[RHPilots]) -> TPilotResults:
+    if p is None:
+        return []
+
+    pilot_results = {
+            pilot.pilot_id:
+            TPilotResult(
+                rank = 1,
+                nickname=pilot.callsign,
+                points = BASE_POINTS,
+                fastest_lap = NO_DATA,
+                fastest_lap_source = NO_DATA,
+                fastest_3_laps = NO_DATA,
+                fastest_3_laps_source = NO_DATA,
+                next_heat = NO_DATA,
+                )
+            for pilot in p.pilots
+        }
+
+    if r and r.event_leaderboard:
+        # Fill in fastest_lap column
+        for entry in r.event_leaderboard.by_fastest_lap:
+            pilot_results[entry.pilot_id].fastest_lap = entry.fastest_lap
+            source = entry.fastest_lap_source.displayname if entry.fastest_lap_source else NO_DATA
+            pilot_results[entry.pilot_id].fastest_lap_source = source
+
+        # Fill in consecutives column
+        for entry in r.event_leaderboard.by_consecutives:
+            pilot_results[entry.pilot_id].fastest_3_laps = entry.consecutives
+            source = entry.consecutives_source.displayname if entry.consecutives_source else NO_DATA
+            pilot_results[entry.pilot_id].fastest_3_laps_source = source
+
+        # Fill in points column
+        for entry in r.event_leaderboard.by_race_time:
+            points = entry.points if entry.points else 0
+            pilot_results[entry.pilot_id].points = 1000 + points
+
+    # Fill in next_heat
+    if h:
+        all_heat_ids = set([h.id for h in h.heats])
+        if not r:
+            heats_with_leaderboard = set()
+        else:
+            heats_with_leaderboard = set([int(heat_id) for heat_id in r.heats.keys()])
+        non_flown_heats = all_heat_ids - heats_with_leaderboard
+        for non_flown_heat_id in sorted(list(non_flown_heats), reverse=True):
+            heat = next((h for h in h.heats if h.id == non_flown_heat_id))
+            assert heat is not None
+            for slot in heat.slots:
+                if slot.pilot_id == 0:
+                    continue
+                pilot_results[slot.pilot_id].next_heat = heat.displayname
+
+    # Update rank. Keep the same amount of points - the same rank
+    results = list(sorted(pilot_results.values(), key=lambda r: r.points, reverse=True))
+    rank = 1
+    for i, _ in enumerate(results):
+        results[i].rank = rank
+        if i+1 < len(results) and results[i+1].points != results[i].points:
+            rank += 1
+
+    return results
+
+
+def rounds(r: Optional[RHResults], h: Optional[RHHeats], c: Optional[RHClasses], p: Optional[RHPilots], f: Optional[RHFrequencies]) -> TRounds:
+    if c is None or h is None or p is None or f is None:
+        return []
+
+    def find_heats_by_class_id(class_id: int):
+        assert h
+        return [h for h in h.heats if h.class_id == class_id]
+    def find_pilot_by_id(pilot_id: int):
+        assert p
+        return next((p for p in p.pilots if p.pilot_id == pilot_id), None)
+    def find_pilot_leaderboard_by_heat_and_pilot_id(heat_id: int, pilot_id: int):
+        if not r or str(heat_id) not in r.heats:
+            return None
+        leaderboard = r.heats[str(heat_id)].leaderboard
+        if leaderboard is None:
+            return None
+        for entry in leaderboard.by_race_time:
+            if entry.pilot_id != pilot_id:
+                continue
+            return entry
+
+        return None
+
+
+    position_to_gains = {
+            1: "50",
+            2: "20",
+            3: "-20",
+            4: "-50"
+    }
+
+    rounds = []
+    for race_class in c.classes:
+        race_round = TRound(round_name=race_class.displayname, heats=[])
+        rh_heats = find_heats_by_class_id(race_class.id)
+        for rh_heat in rh_heats:
+            th = THeat(heat_name=rh_heat.displayname, pilots=[])
+            for rh_slot in rh_heat.slots:
+                if len(f.fdata) <= rh_slot.node_index or f.fdata[rh_slot.node_index].band is None:
+                    continue
+                rhp = find_pilot_by_id(rh_slot.pilot_id)
+
+                # pilot info
+                channel = frequency_to_str(f.fdata[rh_slot.node_index])
+                callsign = value_or_default(rhp.callsign, EMPTY) if rhp is not None else EMPTY
+
+                # result info
+                lb_entry = find_pilot_leaderboard_by_heat_and_pilot_id(rh_heat.id, rh_slot.pilot_id)
+                if lb_entry is not None and rhp is not None:
+                    position = str(value_or_default(lb_entry.position, NO_DATA))
+                    gains = map_or_default(position_to_gains, lb_entry.position, NO_DATA)
+                    points = NO_DATA
+                    if lb_entry.points is not None:
+                        points = str(BASE_POINTS + lb_entry.points)
+                else:
+                    position = NO_DATA
+                    gains = NO_DATA
+                    points = NO_DATA
+
+                th.pilots.append(THeatPilot(
+                        channel=channel,
+                        nickname=callsign,
+                        position=position,
+                        gains=gains,
+                        points=points))
+
+            race_round.heats.append(th)
+
+        rounds.append(race_round)
+
+
+    return rounds
+
+
+
+
+
---
+++ test_converter.py
@@ -0,0 +1,235 @@
+import pytest
+import json
+from RHClassTypes import Classes
+
+from RHHeatTypes import Heats
+from RHRaceStatusTypes import RaceStatus
+from RHPilotTypes import Pilots
+from RHResultTypes import Results
+from RHFrequencyTypes import Frequencies
+from TemplateTypes import PilotResult, HeatPilot
+import converter
+
+@pytest.mark.parametrize("race_status_filename", [
+    "testdata/race_status_data_sample_1.json",
+])
+@pytest.mark.parametrize("heats_filename", [
+    "testdata/heat_data_sample_1.json",
+    "testdata/heat_data_sample_2.json"
+])
+def test_current_heat_converter(race_status_filename, heats_filename):
+    race_status = RaceStatus(**json.load(open(race_status_filename)))
+    heats = Heats(**json.load(open(heats_filename)))
+
+    current_heat = converter.current_heat(race_status, heats)
+
+    assert current_heat == "SundayWhoop Qualifier 3"
+
+@pytest.mark.parametrize("race_status_filename", [
+    "testdata/race_status_data_sample_1.json",
+])
+@pytest.mark.parametrize("heats_filename", [
+    "testdata/heat_data_sample_1.json",
+])
+def test_current_heat_converter_null_heat(race_status_filename, heats_filename):
+    race_status = RaceStatus(**json.load(open(race_status_filename)))
+    heats = Heats(**json.load(open(heats_filename)))
+
+    race_status.race_heat_id = 0
+
+    current_heat = converter.current_heat(race_status, heats)
+
+    assert current_heat == "N/A"
+
+
+@pytest.mark.parametrize("race_status_filename", [
+    "testdata/race_status_data_sample_1.json",
+])
+@pytest.mark.parametrize("heats_filename", [
+    "testdata/heat_data_sample_1.json",
+])
+@pytest.mark.parametrize("pilots_filename", [
+    "testdata/pilot_data_sample_1.json"
+])
+@pytest.mark.parametrize("freq_filename", [
+    "testdata/frequency_data_sample_1.json"
+])
+def test_current_pilots_converter(race_status_filename, heats_filename, pilots_filename, freq_filename):
+    race_status = RaceStatus(**json.load(open(race_status_filename)))
+    heats = Heats(**json.load(open(heats_filename)))
+    pilots = Pilots(**json.load(open(pilots_filename)))
+    frequency = Frequencies(**json.load(open(freq_filename)))
+
+    current_pilots = converter.current_pilots(race_status, heats, pilots, frequency)
+
+    assert len(current_pilots) == 4
+    assert current_pilots[0].nickname == "Armis"
+    assert current_pilots[0].channel == "R2 (5695 Mhz)"
+    assert current_pilots[1].nickname == "BatOnTopas"
+    assert current_pilots[1].channel == "R3 (5732 Mhz)"
+    assert current_pilots[2].nickname == ""
+    assert current_pilots[2].channel == "R6 (5843 Mhz)"
+    assert current_pilots[3].nickname == ""
+    assert current_pilots[3].channel == "R7 (5880 Mhz)"
+
+
+@pytest.mark.parametrize("results_filename", [
+    "testdata/result_data_sample_1.json"
+])
+@pytest.mark.parametrize("heats_filename", [
+    "testdata/heat_data_sample_3.json"
+])
+@pytest.mark.parametrize("pilots_filename", [
+    "testdata/pilot_data_sample_2.json"
+])
+def test_current_pilots_converter_pilot_results(results_filename, heats_filename, pilots_filename):
+    results = Results(**json.load(open(results_filename)))
+    heats = Heats(**json.load(open(heats_filename)))
+    pilots = Pilots(**json.load(open(pilots_filename)))
+
+    pr = converter.pilot_results(results, heats, pilots)
+
+    assert len(pr) == len(pilots.pilots)
+    assert pr[0] == PilotResult(
+            rank=1,
+            nickname='Jauler',
+            points=1050,
+            fastest_lap='0:04.390',
+            fastest_lap_source='QF1',
+            fastest_3_laps='0:38.225',
+            fastest_3_laps_source='QF1',
+            next_heat="N/A")
+    assert pr[1] == PilotResult(
+            rank=2,
+            nickname='Edga',
+            points=1020,
+            fastest_lap='0:06.377',
+            fastest_lap_source='QF1',
+            fastest_3_laps='0:39.122',
+            fastest_3_laps_source='QF1',
+            next_heat="N/A")
+    assert pr[2] == PilotResult(
+            rank=3,
+            nickname='3RC',
+            points=1000,
+            fastest_lap='N/A',
+            fastest_lap_source='N/A',
+            fastest_3_laps='N/A',
+            fastest_3_laps_source='N/A',
+            next_heat="N/A")
+    assert pr[5] == PilotResult(
+            rank=3,
+            nickname='AM14',
+            points=1000,
+            fastest_lap='N/A',
+            fastest_lap_source='N/A',
+            fastest_3_laps='N/A',
+            fastest_3_laps_source='N/A',
+            next_heat='QF2')
+    assert pr[-2] == PilotResult(rank=4,
+            nickname='Armis',
+            points=980,
+            fastest_lap='0:08.048',
+            fastest_lap_source='QF1',
+            fastest_3_laps='0:40.721',
+            fastest_3_laps_source='QF1',
+            next_heat="N/A")
+    assert pr[-1] == PilotResult(
+            rank=5,
+            nickname='Goo',
+            points=950,
+            fastest_lap='0:09.663',
+            fastest_lap_source='QF1',
+            fastest_3_laps='0:41.096',
+            fastest_3_laps_source='QF1',
+            next_heat="N/A")
+
+
+@pytest.mark.parametrize("results_filename", [
+    "testdata/result_data_sample_1.json"
+])
+@pytest.mark.parametrize("heats_filename", [
+    "testdata/heat_data_sample_3.json"
+])
+@pytest.mark.parametrize("pilots_filename", [
+    "testdata/pilot_data_sample_2.json"
+])
+@pytest.mark.parametrize("classes_filename", [
+    "testdata/class_data_sample_2.json"
+])
+@pytest.mark.parametrize("freq_filename", [
+    "testdata/frequency_data_sample_1.json"
+])
+def test_rounds_converter(results_filename, heats_filename, pilots_filename, classes_filename, freq_filename):
+    results = Results(**json.load(open(results_filename)))
+    heats = Heats(**json.load(open(heats_filename)))
+    pilots = Pilots(**json.load(open(pilots_filename)))
+    classes = Classes(**json.load(open(classes_filename)))
+    frequency = Frequencies(**json.load(open(freq_filename)))
+
+    pr = converter.rounds(results, heats, classes, pilots, frequency)
+
+    assert len(pr) == 4
+    assert len(pr[0].heats) == 2
+    assert len(pr[1].heats) == 0
+    assert len(pr[2].heats) == 0
+    assert len(pr[3].heats) == 0
+    assert len(pr[0].heats[0].pilots) == 4
+    assert pr[0].heats[0].pilots == [
+        HeatPilot(
+            channel='R2 (5695 Mhz)',
+            nickname='Jauler',
+            position='1',
+            gains='50',
+            points='1050'
+        ),
+        HeatPilot(
+            channel='R3 (5732 Mhz)',
+            nickname='Edga',
+            position='2',
+            gains='20',
+            points='1020'
+        ),
+        HeatPilot(
+            channel='R6 (5843 Mhz)',
+            nickname='Armis',
+            position='3',
+            gains='-20',
+            points='980'
+        ),
+        HeatPilot(
+            channel='R7 (5880 Mhz)',
+            nickname='Goo',
+            position='4',
+            gains='-50',
+            points='950'
+        )
+    ]
+    assert pr[0].heats[1].pilots == [
+        HeatPilot(channel='R2 (5695 Mhz)',
+            nickname='Goreez',
+            position='N/A',
+            gains='N/A',
+            points='N/A'
+        ),
+        HeatPilot(channel='R3 (5732 Mhz)',
+            nickname='AM14',
+            position='N/A',
+            gains='N/A',
+            points='N/A'
+        ),
+        HeatPilot(channel='R6 (5843 Mhz)',
+            nickname='ChirpFPV',
+            position='N/A',
+            gains='N/A',
+            points='N/A'
+        ),
+        HeatPilot(channel='R7 (5880 Mhz)',
+            nickname='Dainius',
+            position='N/A',
+            gains='N/A',
+            points='N/A'
+        )
+    ]
+
+
